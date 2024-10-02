1c1a394f508f2660b3efd982294416da667eda5d
Kopec9 <kopecmar69@gmail.com>
2024-10-02 15:14:31 +0200

    init_project

---
+++ src/main/java/org/example/nnpda_sema_kopecky/NnpdaSemAKopeckyApplication.java
@@ -0,0 +1,13 @@
+package org.example.nnpda_sema_kopecky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpdaSemAKopeckyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpdaSemAKopeckyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/nnpda_sema_kopecky/NnpdaSemAKopeckyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nnpda_sema_kopecky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpdaSemAKopeckyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
